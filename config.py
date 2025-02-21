f5907afc2159d3c7361329e4bbff2f1e326e74aa
akaahmadaka <137176438+akaahmadaka@users.noreply.github.com>
2025-02-21 14:53:40 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 def get_bot_token():
     """Retrieves the bot token from environment variables or defaults."""
-    token = os.getenv("BOT_TOKEN", "7856502542:AAFOlqazGOG0JpwpQjfmm_ARNd4huACWVpM")
+    token = os.getenv("BOT_TOKEN", "")
     if not token:
         logger.error("BOT_TOKEN environment variable is not set.")
         raise ValueError("BOT_TOKEN environment variable is not set.")
@@ -13,7 +13,7 @@ def get_bot_token():
 
 def get_admin_user_id():
     """Retrieves the admin user ID from environment variables or defaults."""
-    admin_id = os.getenv("ADMIN_USER_ID", "5250831809")
+    admin_id = os.getenv("ADMIN_USER_ID", "")
     if not admin_id:
         logger.error("ADMIN_USER_ID environment variable is not set.")
         raise ValueError("ADMIN_USER_ID environment variable is not set.")
@@ -21,7 +21,7 @@ def get_admin_user_id():
 
 def get_group_link():
     """Retrieves the group link from environment variables or defaults."""
-    group_link = os.getenv("GROUP_LINK", "https://t.me/PAWSOG_bot/PAWS?startapp=x4I1Iegd")
+    group_link = os.getenv("GROUP_LINK", "")
     if not group_link:
         logger.error("GROUP_LINK environment variable is not set.")
         raise ValueError("GROUP_LINK environment variable is not set.")
